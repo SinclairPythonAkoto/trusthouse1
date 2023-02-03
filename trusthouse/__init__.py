b94ad005c4433f701d61d81196bb47cf3eb4f1f3
SinclairPythonAkoto <sinclair.python@gmail.com>
2023-02-03 01:48:25 +0000

    first branch commit: updating __init__ file to create uploads folder (for pics & vids) automatically

--- trusthouse/__init__.py
+++ trusthouse/__init__.py
@@ -1,8 +1,14 @@
+import os
 from flask import Flask
 
 def create_app() -> Flask:
     app: Flask = Flask(__name__)
     # Add your application configurations here
+    app.config["UPLOAD_FOLDER"] = os.path.join(app.root_path, "uploads")
+
+    # create the uploads folder if it doesn't exist
+    if not os.path.exists(app.config["UPLOAD_FOLDER"]):
+        os.makedirs(app.config["UPLOAD_FOLDER"])
     return app
 
 app: create_app = create_app()
